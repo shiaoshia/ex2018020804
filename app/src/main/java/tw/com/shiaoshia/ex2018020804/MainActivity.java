24939f152d99a0c3603c5b27852ad5e4fcc3d871
shiaoshia <34782716+shiaoshia@users.noreply.github.com>
2018-02-08 15:37:22 +0800

    BroadcastReceive 監聽螢幕電源打開filter.addAction(Intent.ACTION_SCREEN_ON);

---
+++ app/src/main/java/tw/com/shiaoshia/ex2018020804/MainActivity.java
@@ -0,0 +1,50 @@
+package tw.com.shiaoshia.ex2018020804;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    MyReceiver receiver;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        receiver = new MyReceiver();
+    }
+
+    //註冊receiver
+    public void click01(View v) {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("myaction");
+        filter.setPriority(500);
+        registerReceiver(receiver,filter);
+        Log.d("MainActivity:","註冊recivier");
+    }
+
+    //取消receiver
+    public void click02(View v) {
+        unregisterReceiver(receiver);
+        Log.d("MainActivity:","取消recivier");
+    }
+
+    //寄出訊號
+    public void click03(View v) {
+        Intent intent = new Intent("myaction");
+        sendBroadcast(intent);
+        Log.d("MainActivity:","寄出訊號");
+    }
+
+    //監聽螢幕是否打開
+    public void click04(View v) {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_SCREEN_ON);
+        filter.setPriority(500);
+        registerReceiver(receiver,filter);
+        Log.d("MainActivity:","註冊recivier");
+    }
+}
---
+++ app/src/main/java/tw/com/shiaoshia/ex2018020804/MyReceiver.java
@@ -0,0 +1,17 @@
+package tw.com.shiaoshia.ex2018020804;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        //throw new UnsupportedOperationException("Not yet implemented");
+        Log.d("MyReceiver","onReceive接收訊號");
+    }
+}
